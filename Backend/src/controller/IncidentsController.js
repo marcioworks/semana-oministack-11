f981d7a68d4369c3c8ffc65a991047b8256fadb5
Marcio silva <marcioadsworks@gmail.com>
2020-04-01 22:16:49 -0300

    delete incident

--- Backend/src/controller/IncidentsController.js
+++ Backend/src/controller/IncidentsController.js
@@ -1,24 +1,45 @@
 const connection = require('../database/connection')
 
 module.exports = {
-    async index(request,response){
+    async index(request, response) {
         const incidents = await connection('incidents').select('*')
 
+        
         return response.json(incidents)
     },
 
 
     async create(request, response) {
-        const { title, description, value } = request.body
-        const ong_id = request.headers.authorization
+        const { title, description, value } = request.body;
+        const ong_id = request.headers.authorization;
 
         const [id] = await connection('incidents').insert({
             title,
             description,
-            ong_id,
             value,
-        })
+            ong_id,
+        });
 
         return response.json({ id })
+    },
+
+    async delete(request, response) {
+        const { id } = request.params
+        const ong_id = request.headers.authorization;
+
+        const incident = await connection('incidents') 
+            .where('id', id)
+            .select('ong_id')
+            .first();
+
+            console.log(incident)
+        if (incident.ong_id !== ong_id) {
+            return response.status(401).json({ error: 'Operation not permited' })
+        }
+
+        
+        await connection('incidents').where('id',id).delete();
+        
+        return response.status(204).send()
     }
 }
