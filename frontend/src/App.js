f939cc2493e122bb48b28d50a938aa7d6d92bbbc
Marcio silva <marcioadsworks@gmail.com>
2020-04-07 11:47:24 -0300

    page register created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-import Logon from './pages/logon'
 import './global.css'
+
+import Routes from './routes'
 
 
 function App() {
 
   return (
-   <Logon/>
+   <Routes/>
       
   );
 }
