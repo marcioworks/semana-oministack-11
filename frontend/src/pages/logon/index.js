6435c240ce58530772583ca35e2f590e16395cd1
Marcio silva <marcioadsworks@gmail.com>
2020-04-09 14:57:37 -0300

    insert and login working

--- frontend/src/pages/logon/index.js
+++ frontend/src/pages/logon/index.js
@@ -1,21 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FiLogIn } from 'react-icons/fi'
-import {Link} from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+
+import api from '../../services/api'
 
 import './style.css'
 
 import heroesImg from '../../assets/SSBU_Hero.jpg'
 import logoImg from '../../assets/behero.png'
 export default function Logon() {
+    const [id, setId] = useState('')
+
+    const history = useHistory();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        try {
+            const response = await api.post('sessions', { id })
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('OngName', response.data.name)
+            history.push('/profile');
+        } catch (err) {
+            alert('falha no login, tente novamente.')
+        }
+
+    }
 
     return (
         <div className="logon-container">
             <section className="form">
-                <img src={logoImg} width="250px" alt="be the hero"/>
+                <img src={logoImg} width="250px" alt="be the hero" />
 
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu Logon</h1>
-                    <input placeholder="Sua ID" />
+                    <input placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
                     <button className="button" type="submit">Entrar</button>
 
                     <Link className="back-link" to="/register">
@@ -25,7 +47,7 @@ export default function Logon() {
                 </form>
 
             </section>
-            <img src={heroesImg} width="860px"  alt="Heroes" />
+            <img src={heroesImg} width="860px" alt="Heroes" />
         </div>
     );
 }
